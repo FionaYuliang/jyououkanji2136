ddf69898d3ffd1ecaa59e2a880402163cda61e43
yangmingming <yunmengze210@163.com>
2023-10-31 22:19:31 +0800

    feat:添加数据内容

---
+++ src/pages/content.ts
@@ -0,0 +1,1181 @@
+let id = 0;
+function getId() {
+  id++;
+  return id;
+}
+let hanziId = 0;
+function getHanziId() {
+  hanziId++;
+  return hanziId;
+}
+
+export const hiraganaContentList = [
+  {
+    title: "あア",
+    list: [
+      {
+        name: "あ",
+        id: getId(),
+        child: [
+          {
+            read: "亜",
+            hanziId: 0001,
+            word:["亜熱帯・あねつたい","亜麻あま","亜流・ありゅう"]
+            xundu:"亜ぐ・つぐ",
+          },
+        ],
+      },
+      {
+        name: "あい",
+        id: getId(),
+        child: [
+          {
+            hanzi: "哀",
+            hanziId: 0002,
+            word:["哀歓・あいかん","",""]
+            xundu:"哀れ・あわれ",
+          },
+          {
+            hanzi: "挨",
+            hanziId: 0003,
+            word:["挨拶・あいさつ","",""]
+            xundu:"",
+          },
+          {
+            hanzi: "愛",
+            hanziId: 0004,
+            word:["愛好・あいこう","愛らしい","愛着・あいちゃく"]
+            xundu:"",
+          },
+          {
+            hanzi: "曖",
+            hanziId: 0005,
+            word:["曖昧・あいまい"]
+            xundu:"",
+          },
+        ],
+      },
+      {
+        name: "あく",
+        id: getId(),
+        child: [
+          {
+            hanzi: "",
+            hanziId: 0006,
+            word:["","",""]
+            xundu:"",
+          },
+          {
+            hanzi: "",
+            hanziId: 0007,
+            word:["","",""]
+            xundu:"",
+          },
+        ],
+      },
+      {
+        name: "あつ",
+        id: getId(),
+        child: [
+          {
+            hanzi: "",
+            hanziId: 0008,
+            word:["","",""]
+            xundu:"",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    title: "いイ",
+    list: [
+      {
+        name: "い",
+        id: getId(),
+      },
+      {
+        name: "いき",
+        id: getId(),
+      },
+      {
+        name: "いく",
+        id: getId(),
+      },
+      {
+        name: "いち",
+        id: getId(),
+      },
+      {
+        name: "いつ",
+        id: getId(),
+      },
+      {
+        name: "いばら",
+        id: getId(),
+      },
+      {
+        name: "いも",
+        id: getId(),
+      },
+      {
+        name: "いん",
+        id: getId(),
+      },
+    ],
+  },
+  {
+    title: "うウ",
+    list: [
+      {
+        name: "う",
+        id: getId(),
+      },
+      {
+        name: "うた",
+        id: getId(),
+      },
+      {
+        name: "うつ",
+        id: getId(),
+      },
+      {
+        name: "うね",
+        id: getId(),
+      },
+      {
+        name: "うら",
+        id: getId(),
+      },
+      {
+        name: "うん",
+        id: getId(),
+      },
+    ],
+  },
+  {
+    title: "えエ",
+    list: [
+      {
+        name: "えい",
+        id: getId(),
+      },
+      {
+        name: "えき",
+        id: getId(),
+      },
+      {
+        name: "えつ",
+        id: getId(),
+      },
+      {
+        name: "えん",
+        id: getId(),
+      },
+    ],
+  },
+  {
+    title: "おオ",
+    list: [
+      {
+        name: "お",
+        id: getId(),
+      },
+      {
+        name: "おう",
+        id: getId(),
+      },
+      {
+        name: "おか",
+        id: getId(),
+      },
+      {
+        name: "おく",
+        id: getId(),
+      },
+      {
+        name: "おそれ",
+        id: getId(),
+      },
+      {
+        name: "おつ",
+        id: getId(),
+      },
+      {
+        name: "おれ",
+        id: getId(),
+      },
+      {
+        name: "おろす",
+        id: getId(),
+      },
+      {
+        name: "おん",
+        id: getId(),
+      },
+    ],
+  },
+  {
+    title: "かカ",
+    list: [
+      {
+        name: "か",
+        id: getId(),
+      },
+      {
+        name: "が",
+        id: getId(),
+      },
+      {
+        name: "かい",
+        id: getId(),
+      },
+      {
+        name: "がい",
+        id: getId(),
+      },
+      {
+        name: "かき",
+        id: getId(),
+      },
+      {
+        name: "かき",
+        id: getId(),
+      },
+      {
+        name: "かく",
+        id: getId(),
+      },
+      {
+        name: "がく",
+        id: getId(),
+      },
+      {
+        name: "かける",
+        id: getId(),
+      },
+      {
+        name: "かた",
+        id: getId(),
+      },
+      {
+        name: "かつ",
+        id: getId(),
+      },
+      {
+        name: "かぶ",
+        id: getId(),
+      },
+      {
+        name: "かま",
+        id: getId(),
+      },
+      {
+        name: "かま",
+        id: getId(),
+      },
+      {
+        name: "かる",
+        id: getId(),
+      },
+      {
+        name: "かん",
+        id: getId(),
+      },
+      {
+        name: "がん",
+        id: getId(),
+      },
+    ],
+  },
+  {
+    title: "きキ",
+    list: [
+      {
+        name: "き",
+        id: getId(),
+      },
+      {
+        name: "ぎ",
+        id: getId(),
+      },
+      {
+        name: "きく",
+        id: getId(),
+      },
+      {
+        name: "きち",
+        id: getId(),
+      },
+      {
+        name: "きつ",
+        id: getId(),
+      },
+      {
+        name: "きゃく",
+        id: getId(),
+      },
+      {
+        name: "ぎゃく",
+        id: getId(),
+      },
+      {
+        name: "きゅう",
+        id: getId(),
+      },
+      {
+        name: "きょ",
+        id: getId(),
+      },
+      {
+        name: "ぎょ",
+        id: getId(),
+      },
+      {
+        name: "きょう",
+        id: getId(),
+      },
+      {
+        name: "ぎょう",
+        id: getId(),
+      },
+      {
+        name: "きょく",
+        id: getId(),
+      },
+      {
+        name: "ぎょく",
+        id: getId(),
+      },
+      {
+        name: "きん",
+        id: getId(),
+      },
+      {
+        name: "ぎん",
+        id: getId(),
+      },
+    ],
+  },
+  {
+    title: "くク",
+    list: [
+      {
+        name: "く",
+        id: getId(),
+      },
+      {
+        name: "ぐ",
+        id: getId(),
+      },
+      {
+        name: "くう",
+        id: getId(),
+      },
+      {
+        name: "ぐう",
+        id: getId(),
+      },
+      {
+        name: "くし",
+        id: getId(),
+      },
+      {
+        name: "くつ",
+        id: getId(),
+      },
+      {
+        name: "くま",
+        id: getId(),
+      },
+      {
+        name: "くる",
+        id: getId(),
+      },
+      {
+        name: "くん",
+        id: getId(),
+      },
+      {
+        name: "ぐん",
+        id: getId(),
+      },
+    ],
+  },
+  {
+    title: "けケ",
+    list: [
+      {
+        name: "けい",
+        id: getId(),
+      },
+      {
+        name: "げい",
+        id: getId(),
+      },
+      {
+        name: "げき",
+        id: getId(),
+      },
+      {
+        name: "けた",
+        id: getId(),
+      },
+      {
+        name: "けつ",
+        id: getId(),
+      },
+      {
+        name: "げつ",
+        id: getId(),
+      },
+      {
+        name: "けん",
+        id: getId(),
+      },
+      {
+        name: "げん",
+        id: getId(),
+      },
+    ],
+  },
+  {
+    title: "こコ",
+    list: [
+      {
+        name: "こ",
+        id: getId(),
+      },
+      {
+        name: "ご",
+        id: getId(),
+      },
+      {
+        name: "こう",
+        id: getId(),
+      },
+      {
+        name: "ごう",
+        id: getId(),
+      },
+      {
+        name: "こく",
+        id: getId(),
+      },
+      {
+        name: "ごく",
+        id: getId(),
+      },
+      {
+        name: "こつ",
+        id: getId(),
+      },
+      {
+        name: "こま",
+        id: getId(),
+      },
+      {
+        name: "こむ",
+        id: getId(),
+      },
+      {
+        name: "ころ",
+        id: getId(),
+      },
+      {
+        name: "こん",
+        id: getId(),
+      },
+    ],
+  },
+  {
+    title: "さサ",
+    list: [
+      {
+        name: "さ",
+        id: getId(),
+      },
+      {
+        name: "ざ",
+        id: getId(),
+      },
+      {
+        name: "さい",
+        id: getId(),
+      },
+      {
+        name: "ざい",
+        id: getId(),
+      },
+      {
+        name: "さき",
+        id: getId(),
+      },
+      {
+        name: "さく",
+        id: getId(),
+      },
+      {
+        name: "さつ",
+        id: getId(),
+      },
+      {
+        name: "さく",
+        id: getId(),
+      },
+      {
+        name: "ざつ",
+        id: getId(),
+      },
+      {
+        name: "さら",
+        id: getId(),
+      },
+      {
+        name: "さん",
+        id: getId(),
+      },
+      {
+        name: "ざん",
+        id: getId(),
+      },
+    ],
+  },
+  {
+    title: "しシ",
+    list: [
+      {
+        name: "し",
+        id: getId(),
+      },
+      {
+        name: "じ",
+        id: getId(),
+      },
+      {
+        name: "しか",
+        id: getId(),
+      },
+      {
+        name: "しき",
+        id: getId(),
+      },
+      {
+        name: "じく",
+        id: getId(),
+      },
+      {
+        name: "しち",
+        id: getId(),
+      },
+      {
+        name: "しつ",
+        id: getId(),
+      },
+      {
+        name: "じつ",
+        id: getId(),
+      },
+      {
+        name: "しば",
+        id: getId(),
+      },
+      {
+        name: "しゃ",
+        id: getId(),
+      },
+      {
+        name: "じゃ",
+        id: getId(),
+      },
+      {
+        name: "しゃく",
+        id: getId(),
+      },
+      {
+        name: "じゃく",
+        id: getId(),
+      },
+      {
+        name: "しゅ",
+        id: getId(),
+      },
+      {
+        name: "じゅ",
+        id: getId(),
+      },
+      {
+        name: "しゅう",
+        id: getId(),
+      },
+      {
+        name: "じゅう",
+        id: getId(),
+      },
+      {
+        name: "しゅく",
+        id: getId(),
+      },
+      {
+        name: "じゅく",
+        id: getId(),
+      },
+      {
+        name: "しゅつ",
+        id: getId(),
+      },
+      {
+        name: "じゅつ",
+        id: getId(),
+      },
+      {
+        name: "しゅん",
+        id: getId(),
+      },
+      {
+        name: "じゅん",
+        id: getId(),
+      },
+      {
+        name: "しょ",
+        id: getId(),
+      },
+      {
+        name: "じょ",
+        id: getId(),
+      },
+      {
+        name: "しょう",
+        id: getId(),
+      },
+      {
+        name: "じょう",
+        id: getId(),
+      },
+      {
+        name: "しょく",
+        id: getId(),
+      },
+      {
+        name: "じょく",
+        id: getId(),
+      },
+      {
+        name: "しり",
+        id: getId(),
+      },
+      {
+        name: "しん",
+        id: getId(),
+      },
+      {
+        name: "じん",
+        id: getId(),
+      },
+    ],
+  },
+  {
+    title: "すス",
+    list: [
+      {
+        name: "す",
+        id: getId(),
+      },
+      {
+        name: "ず",
+        id: getId(),
+      },
+      {
+        name: "すい",
+        id: getId(),
+      },
+      {
+        name: "ずい",
+        id: getId(),
+      },
+      {
+        name: "すう",
+        id: getId(),
+      },
+      {
+        name: "すえる",
+        id: getId(),
+      },
+      {
+        name: "すぎ",
+        id: getId(),
+      },
+      {
+        name: "すそ",
+        id: getId(),
+      },
+      {
+        name: "すん",
+        id: getId(),
+      },
+    ],
+  },
+  {
+    title: "せセ",
+    list: [
+      {
+        name: "せ",
+        id: getId(),
+      },
+      {
+        name: "ぜ",
+        id: getId(),
+      },
+      {
+        name: "せい",
+        id: getId(),
+      },
+      {
+        name: "ぜい",
+        id: getId(),
+      },
+      {
+        name: "せき",
+        id: getId(),
+      },
+      {
+        name: "せつ",
+        id: getId(),
+      },
+      {
+        name: "ぜつ",
+        id: getId(),
+      },
+      {
+        name: "せん",
+        id: getId(),
+      },
+      {
+        name: "ぜん",
+        id: getId(),
+      },
+    ],
+  },
+  {
+    title: "そソ",
+    list: [
+      {
+        name: "そ",
+        id: getId(),
+      },
+      {
+        name: "そう",
+        id: getId(),
+      },
+      {
+        name: "ぞう",
+        id: getId(),
+      },
+      {
+        name: "そく",
+        id: getId(),
+      },
+      {
+        name: "ぞく",
+        id: getId(),
+      },
+      {
+        name: "そつ",
+        id: getId(),
+      },
+      {
+        name: "そん",
+        id: getId(),
+      },
+    ],
+  },
+  {
+    title: "たタ",
+    list: [
+      {
+        name: "た",
+        id: getId(),
+      },
+      {
+        name: "だ",
+        id: getId(),
+      },
+      {
+        name: "たい",
+        id: getId(),
+      },
+      {
+        name: "だい",
+        id: getId(),
+      },
+      {
+        name: "たき",
+        id: getId(),
+      },
+      {
+        name: "たく",
+        id: getId(),
+      },
+      {
+        name: "だく",
+        id: getId(),
+      },
+      {
+        name: "ただし",
+        id: getId(),
+      },
+      {
+        name: "たつ",
+        id: getId(),
+      },
+      {
+        name: "だつ",
+        id: getId(),
+      },
+      {
+        name: "たな",
+        id: getId(),
+      },
+      {
+        name: "だれ",
+        id: getId(),
+      },
+      {
+        name: "たん",
+        id: getId(),
+      },
+      {
+        name: "だん",
+        id: getId(),
+      },
+    ],
+  },
+  {
+    title: "ちチ",
+    list: [
+      {
+        name: "ち",
+        id: getId(),
+      },
+      {
+        name: "ちく",
+        id: getId(),
+      },
+      {
+        name: "ちつ",
+        id: getId(),
+      },
+      {
+        name: "ちゃ",
+        id: getId(),
+      },
+      {
+        name: "ちゃく",
+        id: getId(),
+      },
+      {
+        name: "ちゅう",
+        id: getId(),
+      },
+      {
+        name: "ちょ",
+        id: getId(),
+      },
+      {
+        name: "ちょう",
+        id: getId(),
+      },
+      {
+        name: "ちょく",
+        id: getId(),
+      },
+      {
+        name: "ちん",
+        id: getId(),
+      },
+    ],
+  },
+  {
+    title: "つツ",
+    list: [
+      {
+        name: "つい",
+        id: getId(),
+      },
+      {
+        name: "つい",
+        id: getId(),
+      },
+      {
+        name: "つう",
+        id: getId(),
+      },
+      {
+        name: "つか",
+        id: getId(),
+      },
+      {
+        name: "つける",
+        id: getId(),
+      },
+      {
+        name: "つば",
+        id: getId(),
+      },
+      {
+        name: "つめ",
+        id: getId(),
+      },
+      {
+        name: "つる",
+        id: getId(),
+      },
+    ],
+  },
+  {
+    title: "てテ",
+    list: [
+      {
+        name: "辽宁",
+        id: getId(),
+      },
+    ],
+  },
+  {
+    title: "とト",
+    list: [
+      {
+        name: "辽宁",
+        id: getId(),
+      },
+    ],
+  },
+  {
+    title: "なナ",
+    list: [
+      {
+        name: "辽宁",
+        id: getId(),
+      },
+    ],
+  },
+  {
+    title: "にニ",
+    list: [
+      {
+        name: "辽宁",
+        id: getId(),
+      },
+    ],
+  },
+  {
+    title: "ぬヌ",
+    list: [
+      {
+        name: "辽宁",
+        id: getId(),
+      },
+    ],
+  },
+  {
+    title: "ねネ",
+    list: [
+      {
+        name: "辽宁",
+        id: getId(),
+      },
+    ],
+  },
+  {
+    title: "のノ",
+    list: [
+      {
+        name: "辽宁",
+        id: getId(),
+      },
+    ],
+  },
+  {
+    title: "はハ",
+    list: [
+      {
+        name: "辽宁",
+        id: getId(),
+      },
+    ],
+  },
+  {
+    title: "ひヒ",
+    list: [
+      {
+        name: "辽宁",
+        id: getId(),
+      },
+    ],
+  },
+  {
+    title: "ふフ",
+    list: [
+      {
+        name: "辽宁",
+        id: getId(),
+      },
+    ],
+  },
+  {
+    title: "へヘ",
+    list: [
+      {
+        name: "辽宁",
+        id: getId(),
+      },
+    ],
+  },
+  {
+    title: "ほホ",
+    list: [
+      {
+        name: "辽宁",
+        id: getId(),
+      },
+    ],
+  },
+  {
+    title: "まマ",
+    list: [
+      {
+        name: "辽宁",
+        id: getId(),
+      },
+    ],
+  },
+  {
+    title: "みミ",
+    list: [
+      {
+        name: "辽宁",
+        id: getId(),
+      },
+    ],
+  },
+  {
+    title: "むム",
+    list: [
+      {
+        name: "辽宁",
+        id: getId(),
+      },
+    ],
+  },
+  {
+    title: "めメ",
+    list: [
+      {
+        name: "辽宁",
+        id: getId(),
+      },
+    ],
+  },
+  {
+    title: "もモ",
+    list: [
+      {
+        name: "辽宁",
+        id: getId(),
+      },
+    ],
+  },
+  {
+    title: "まマ",
+    list: [
+      {
+        name: "辽宁",
+        id: getId(),
+      },
+    ],
+  },
+  {
+    title: "やヤ",
+    list: [
+      {
+        name: "辽宁",
+        id: getId(),
+      },
+    ],
+  },
+  {
+    title: "ゆユ",
+    list: [
+      {
+        name: "辽宁",
+        id: getId(),
+      },
+    ],
+  },
+  {
+    title: "よヨ",
+    list: [
+      {
+        name: "辽宁",
+        id: getId(),
+      },
+    ],
+  },
+  {
+    title: "らラ",
+    list: [
+      {
+        name: "辽宁",
+        id: getId(),
+      },
+    ],
+  },
+
+  {
+    title: "りリ",
+    list: [
+      {
+        name: "辽宁",
+        id: getId(),
+      },
+    ],
+  },
+
+  {
+    title: "るル",
+    list: [
+      {
+        name: "辽宁",
+        id: getId(),
+      },
+    ],
+  },
+
+  {
+    title: "れレ",
+    list: [
+      {
+        name: "辽宁",
+        id: getId(),
+      },
+    ],
+  },
+  {
+    title: "ろロ",
+    list: [
+      {
+        name: "辽宁",
+        id: getId(),
+      },
+    ],
+  },
+  {
+    title: "わワ",
+    list: [
+      {
+        name: "辽宁",
+        id: getId(),
+      },
+    ],
+  },
+];
